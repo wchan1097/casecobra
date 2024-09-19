e63009b27c5f22a633f4fefaff5c216fe2ffbc7d
William <il.wchan97@gmail.com>
2024-09-19 18:56:24 -0500

    stars and lucide react add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,93 @@
 import Image from "next/image";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Check, Star } from "lucide-react";
 
 export default function Home() {
   return (
     <div className="bg-slate-50 text-black bg-white">
 		<section>
-			<MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
+			<MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 
+			xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
 				<div className="col-span-2 px-6 lg:px-0 lg:pt-4">
-					<div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
-						<div className="absolute w-28 left-0 top-0 hidden lg:block">
+					<div className="relative mx-auto text-center lg:text-left flex flex-col items-center
+					lg:items-start">
+						<div className="absolute w-28 left-0 -top-0 hidden lg:block">
 							<img src="/snake-1.png" className="w-full"></img>
 						</div>
-						<h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
-							Your Image On A <span className="bg-green-600 px-2 text-white rounded">Custom</span> Phone Case
+						<h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold 
+						!leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
+							Your Image On A 
+							<span className="bg-green-600 px-2 mx-2 text-white rounded">Custom</span> 
+							Phone Case
 						</h1>
-						<p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
+						<p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left 
+						text-balance md:text-wrap">
 							Capture your favorite moments with your own, 
 							<span className="font-semibold"> one-of-one </span> 
-							phone case. CaseCobra allows you to protect your memories, not just your phone.
+							phone case. CaseCobra allows you to protect your memories, not just your 
+							phone.
 						</p>
+
+						<ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center
+						sm:items-start">
+							<div className="space-y-2">
+								<li className="flex gap-1.5 items-center text-left">
+									<Check className="h-5 w-5 shrink-0 text-green-600"/>
+									High-quality, durable material
+								</li>
+								<li className="flex gap-1.5 items-center text-left">
+									<Check className="h-5 w-5 shrink-0 text-green-600"/>
+									5 year print guarantee
+								</li>
+								<li className="flex gap-1.5 items-center text-left">
+									<Check className="h-5 w-5 shrink-0 text-green-600"/>
+									Modern iPhone models supported
+								</li>
+							</div>
+						</ul>
+
+						<div className="mt-12 flex flex-col sm:flex-row items-center
+						sm:items-start gap-5">
+							<div className="flex -space-x-4">
+								<img 
+									className="inline-block object-cover h-10 w-10 rounded-full ring-2 ring-slate-2"
+									src='/users/user-1.png'
+									alt='User image'
+								/>
+								<img 
+									className="inline-block object-cover h-10 w-10 rounded-full ring-2 ring-slate-2"
+									src='/users/user-2.png'
+									alt='User image'
+								/>
+								<img 
+									className="inline-block object-cover h-10 w-10 rounded-full ring-2 ring-slate-2"
+									src='/users/user-3.png'
+									alt='User image'
+								/>
+								<img 
+									className="inline-block object-cover h-10 w-10 rounded-full ring-2 ring-slate-2"
+									src='/users/user-4.jpg'
+									alt='User image'
+								/>
+								<img 
+									className="inline-block object-cover h-10 w-10 rounded-full ring-2 ring-slate-2"
+									src='/users/user-5.jpg'
+									alt='User image'
+								/>
+							</div>
+
+							<div className="flex flex-col justify-between items-center sm:items-start">
+								<div className="flex gap-0.5">
+									<Star className="h-4 w-4 text-green-600 fill-green-600" />
+									<Star className="h-4 w-4 text-green-600 fill-green-600" />
+									<Star className="h-4 w-4 text-green-600 fill-green-600" />
+									<Star className="h-4 w-4 text-green-600 fill-green-600" />
+									<Star className="h-4 w-4 text-green-600 fill-green-600" />
+								</div>
+							</div>
+
+							<p><span className="font-semibold">1,250</span> Happy Customers</p>
+						</div>
 					</div>
 				</div>
 			</MaxWidthWrapper>
