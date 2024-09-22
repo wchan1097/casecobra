0381de4d9f8f65a52c3a05cb908e90046419d91a
William <il.wchan97@gmail.com>
2024-09-22 11:49:23 -0500

    finish nav bar display

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { buttonVariants } from "./ui/button";
+import { ArrowRight } from "lucide-react";
 
 const NavBar = () => {
 	const user = undefined;
@@ -24,9 +25,8 @@ const NavBar = () => {
 									className={buttonVariants({
 										size: "sm", 
 										variant: "ghost"
-									})}
-								>
-									Sign Out
+									})}>
+									Sign out
 								</Link>
 								{ isAdmin ? (
 									<Link 
@@ -35,12 +35,50 @@ const NavBar = () => {
 											size: "sm", 
 											variant: "ghost"
 										})}>
-										Sign Out
+										Dashboard ✨
 									</Link>
 								) : null}
+								<Link 
+									href="/api/auth/logout" 
+									className={buttonVariants({
+										size: "sm", 
+										className: "hidden sm:flex items-center gap-1"
+									})}>
+									Create Case
+									<ArrowRight className="ml-1.5 h-5 w-5"/>
+								</Link>
 							</>
 						) : (
-							<></>
+							<>
+								<Link 
+									href="/api/auth/register" 
+									className={buttonVariants({
+										size: "sm", 
+										variant: "ghost"
+									})}>
+									Sign up
+								</Link>
+								<Link 
+									href="/api/auth/login" 
+									className={buttonVariants({
+										size: "sm", 
+										variant: "ghost"
+									})}>
+									Login
+								</Link>
+
+								<div className="h-8 w-px bg-zinc-200 hidden sm:block"></div>
+
+								<Link 
+									href="/api/auth/logout" 
+									className={buttonVariants({
+										size: "sm", 
+										className: "hidden sm:flex items-center gap-1"
+									})}>
+									Create Case
+									<ArrowRight className="ml-1.5 h-5 w-5"/>
+								</Link>
+							</>
 						)}
 					</div>
 				</div>
